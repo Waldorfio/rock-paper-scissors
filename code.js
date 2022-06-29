f1c33c210b659983ef2dd6061478d8e3558b8f4a
Waldorfio <walid.hanifi707@gmail.com>
2022-06-29 21:18:37 +1000

    Create code skeleton

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 
 arr = ['Rock', 'Paper', 'Scissors'];
-let handIn = 'Rock';
+let handIn = prompt("Please enter Rock, Paper or Scissors")
 let winMsg = 'You Win!';
 let loseMsg = 'You Lose!';
 let drawMsg = 'Draw...';
@@ -10,18 +10,30 @@ function computerPlay() {
     return handOut;
 }
 
-handOut = computerPlay();
+function testResult(a, b) {
+    handIn = a;
+    handOut = b;
 
-if (handIn == 'Rock' && handOut == 'Scissors') {msg = winMsg};
-if (handIn == 'Rock' && handOut == 'Paper') {msg = loseMsg};
-if (handIn == 'Rock' && handOut == 'Rock') {msg = drawMsg};
+    if (handIn == 'Rock' && handOut == 'Scissors') {msg = winMsg};
+    if (handIn == 'Rock' && handOut == 'Paper') {msg = loseMsg};
+    if (handIn == 'Rock' && handOut == 'Rock') {msg = drawMsg};
 
-if (handIn == 'Paper' && handOut == 'Scissors') {msg = loseMsg};
-if (handIn == 'Paper' && handOut == 'Paper') {msg = drawMsg};
-if (handIn == 'Paper' && handOut == 'Rock') {msg = winMsg};
+    if (handIn == 'Paper' && handOut == 'Scissors') {msg = loseMsg};
+    if (handIn == 'Paper' && handOut == 'Paper') {msg = drawMsg};
+    if (handIn == 'Paper' && handOut == 'Rock') {msg = winMsg};
 
-if (handIn == 'Scissors' && handOut == 'Scissors') {msg = drawMsg};
-if (handIn == 'Scissors' && handOut == 'Paper') {msg = winMsg};
-if (handIn == 'Scissors' && handOut == 'Rock') {msg = loseMsg};
+    if (handIn == 'Scissors' && handOut == 'Scissors') {msg = drawMsg};
+    if (handIn == 'Scissors' && handOut == 'Paper') {msg = winMsg};
+    if (handIn == 'Scissors' && handOut == 'Rock') {msg = loseMsg};
 
-alert(msg);
+    return msg;
+}
+
+function playRound(a) {
+    handIn = a;
+    handOut = computerPlay();
+    result = testResult(handIn, handOut);
+    alert(result);
+}
+
+playRound(handIn);
