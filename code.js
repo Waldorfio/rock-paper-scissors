2694fa75e0d50c85d3cabc00d6a3bf656a0be3a2
Waldorfio <walid.hanifi707@gmail.com>
2022-07-10 19:33:51 +1000

    Add interactive messages

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ const arr = ['ROCK', 'PAPER', 'SCISSORS'];
 const winMsg = 'You Win!';
 const loseMsg = 'You Lose!';
 const drawMsg = 'Draw...';
+let score = 0;
 
 function computerPlay() {
     const handOut = arr[Math.floor((Math.random() * 3))]
@@ -33,7 +34,17 @@ function playRound(itemInput) {
     if (handIn != 'ROCK' && handIn != 'PAPER' && handIn != 'SCISSORS') {throw new Error('Invalid input!')};
     handOut = computerPlay();
     result = testResult(handIn, handOut);
-    document.getElementById('results').innerHTML = result;
+    
+    document.getElementById('score').innerHTML = 'Score: ' + score;  // appending results to DOM
+
+    //Running Score
+    if (result = 'You Win!') {score = score + 1};
+    if (score == 5) {'GAME OVER! YOU WIN'};
+    document.getElementById('score').innerHTML = 'Score: ' + score;    // apppending score to DOM
+    if (score == 5) {
+        document.getElementById('results').innerHTML = '';
+        document.getElementById('score').innerHTML = 'GAME OVER... YOU WIN!';
+    };
 }
 
 // Adding the Event Listeners to the buttons
